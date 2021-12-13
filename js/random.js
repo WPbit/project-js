ad112ad1cc8bd90849bb54fb76bc7327fe3c481e
Oleg Alekhnovich <o.alekhnovich@gmail.com>
2021-12-13 23:35:48 +0200

    added random num generator v.1

--- js/random.js
+++ js/random.js
@@ -3,9 +3,6 @@ function generateRandNum()
     let min = +document.querySelector('#inputMin').value;
     let max = +document.querySelector('#inputMax').value;
 
-    console.log(min);
-    console.log(max);
-
     let randNum = Math.round(Math.random() * max);
     let result = randNum < max ? randNum : randNum + min;
 
